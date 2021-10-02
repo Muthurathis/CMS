c1fcc320da153b26b2f7909fb6d5702ac268bd65
Muthurathis <muthurathisari1@gmail.com>
2021-10-02 21:05:37 +0530

    database fix-1

--- includes/db.php
+++ includes/db.php
@@ -2,7 +2,7 @@
 $connection=mysqli_connect('sql304.epizy.com','epiz_29918729','08Fjycbw3fJ','epiz_29918729_Blog');
 
 if(!$connection){
-    echo 'Not connected to database';
+    echo 'Not connected to database'.mysqli_error();
     die();
 }
 
