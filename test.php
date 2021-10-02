b5631827c0ef934a6842267ee283bbe2a4071830
Muthurathis <muthurathisari1@gmail.com>
2021-10-02 22:25:28 +0530

    test 2

--- test.php
+++ test.php
@@ -3,5 +3,7 @@ $connection=mysqli_connect('sql304.bytecluster.com','epiz_29918729','08Fjycbw3fJ
 echo "working ,..";
 $posts_query="SELECT * FROM posts ";
 $post_result=mysqli_query($connection,$posts_query);
-print_r($post_result);
+
+$row=mysqli_fetch_assoc($post_result);
+print_r($row);
 ?>
