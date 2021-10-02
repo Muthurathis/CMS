49585b592c65a46e7b03c6f143ffacdbe7525f6c
Muthurathis <muthurathisari1@gmail.com>
2021-10-02 23:04:20 +0530

    test 3

--- test.php
+++ test.php
@@ -1,9 +1,6 @@
 <?php
 $connection=mysqli_connect('sql304.epizy.com','epiz_29918729','08Fjycbw3fJ','epiz_29918729_Blog');
-if(!$connection){
-    echo 'Not connected to database'.mysqli_connect_error();
-    die();
-}
+
 echo "working ,..";
 $posts_query="SELECT * FROM posts ";
 $post_result=mysqli_query($connection,$posts_query);
