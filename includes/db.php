157e3644cbbb1bf89adc52a8b03d5117cff38b30
Muthurathis <muthurathisari1@gmail.com>
2021-10-02 21:09:23 +0530

    database fix-2

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,16 @@
 <?php
-$connection=mysqli_connect('sql304.epizy.com','epiz_29918729','08Fjycbw3fJ','epiz_29918729_Blog');
+// $connection=mysqli_connect('sql304.epizy.com','epiz_29918729','08Fjycbw3fJ','epiz_29918729_Blog');
+
+
+$host='sql304.epizy.com';
+$user='epiz_29918729';
+$pass='08Fjycbw3fJ';
+$db='epiz_29918729_Blog';
+
+$connection=mysqli_connect($host,$user,$pass,$db);
 
 if(!$connection){
-    echo 'Not connected to database'.mysqli_error();
+    echo 'Not connected to database';
     die();
 }
 
