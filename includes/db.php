deb7bb904b3e1415e48de425dddf30ba52bd9139
Muthurathis <muthurathisari1@gmail.com>
2021-10-02 18:36:11 +0530

    db connection

--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,5 @@
 <?php
-$connection=mysqli_connect('sqlXXX.epizy.com','epiz_29918729','08Fjycbw3fJ','epiz_29918729_Blog');
+$connection=mysqli_connect('sql304.epizy.com','epiz_29918729','08Fjycbw3fJ','epiz_29918729_Blog');
 
 if(!$connection){
     echo 'Not connected to database';
