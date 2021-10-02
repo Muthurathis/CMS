b1e064252726abc28f96fab0124ac7dc4d7de391
Muthurathis <muthurathisari1@gmail.com>
2021-10-02 22:13:14 +0530

    test 3

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
 include './includes/db.php';
 echo "working ,..";
-$posts_query="SELECT * FROM posts WHERE post_id=$post_id ";
+$posts_query="SELECT * FROM posts ";
 $post_result=mysqli_query($connection,$posts_query);
 print_r($post_result);
 ?>
