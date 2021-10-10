65ea082231d2526e6a24d997ebf76be3ebef0fd2
Muthurathis <muthurathisari1@gmail.com>
2021-10-10 07:54:24 +0530

    fix 1

--- test.php
+++ test.php
@@ -1,12 +1,12 @@
 <?php
-$connection=mysqli_connect('sql304.epizy.com','epiz_29918729','08Fjycbw3fJ','epiz_29918729_Blog');
+// $connection=mysqli_connect('sql304.epizy.com','epiz_29918729','08Fjycbw3fJ','epiz_29918729_Blog');
 
 echo "working ,..";
-$posts_query="SELECT * FROM posts ";
-$post_result=mysqli_query($connection,$posts_query);
+// $posts_query="SELECT * FROM posts ";
+// $post_result=mysqli_query($connection,$posts_query);
 
-$row=mysqli_fetch_assoc($post_result);
-print_r($row);
+// $row=mysqli_fetch_assoc($post_result);
+// print_r($row);
 
-print_r($row);
+// print_r($row);
 ?>
